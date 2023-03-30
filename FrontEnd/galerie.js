2f0287d5d6f6f36f0a689e7afaf1d16a5b1050f2
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-03-30 15:48:38 +0200

    prettier

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -9,3 +9,26 @@ function displayConsole(data) {
     console.log(data);
     return data;
 }
+
+function displayGallery(data) {
+    //parent element
+    const gallery = document.querySelector(".gallery");
+
+    for (let i = 0; i < data.length; i++) {
+        const work = data[i];
+
+        //create figure
+        const workCard = document.createElement("figure");
+
+        //create tags
+        const workImage = document.createElement("img");
+        const workTitle = document.createElement("figcaption");
+        workImage.src = work.imageUrl;
+        workTitle.innerText = work.title;
+
+        //references to DOM    
+        gallery.appendChild(workCard);
+        workCard.appendChild(workImage);
+        workCard.appendChild(workTitle);
+    }
+}
