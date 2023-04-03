d98e78404a804a56c2a3d1a1de22df2956efba97
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-04-03 16:48:26 +0200

    WIP gallery filter

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -1,30 +1,85 @@
-// fetch works data from API and display it
-fetch("http://localhost:5678/api/works")
-.then((response) => response.json())
-.then((data) => displayGallery(data))
+const baseApiUrl = "http://localhost:5678/api/";
 
-//function to display the gallery
+// fetch works data from API and display it
+fetch(baseApiUrl + "works")
+.then((response) => response.json())
+.then((worksData) => {
+    //get list of categories
+    let listOfCategories = new Set();
+    worksData.forEach(work => {
+        listOfCategories.add(work.category.name);
+    });
+    categories = Array.from(listOfCategories);
+    //display all works
+    displayGallery(worksData);  
+    //display filter buttons
+    displayFilterButtons(categories);
+})
+
+
+
+//display the gallery
 function displayGallery(data) {
 
     //select parent element
     const gallery = document.querySelector(".gallery");
 
-    //display all works in array
-    for (let i = 0; i < data.length; i++) {
-        const work = data[i];
-
-        //create figure
-        const workCard = document.createElement("figure");
-
+    //show all works in array
+    for (let index = 0; index < data.length; index++) {
+        const work = data[index];
+        
         //create tags
+        const workCard = document.createElement("figure");
         const workImage = document.createElement("img");
         const workTitle = document.createElement("figcaption");
         workImage.src = work.imageUrl;
+        workImage.alt = work.title;
         workTitle.innerText = work.title;
+        workCard.dataset.category = work.category.name;
+        workCard.className = "workCard";
 
         //references to DOM    
         gallery.appendChild(workCard);
-        workCard.appendChild(workImage);
-        workCard.appendChild(workTitle);
+        workCard.append(workImage,workTitle);
     }
 }
+
+//create button all
+function allButton() {
+
+    const filter = document.querySelector(".filter");
+    const button = document.createElement("button");
+    
+    button.innerText = "Tous";
+    button.className = "filterButton";
+    filter.appendChild(button);
+}
+
+//create filter buttons
+function filterButtons(categories) {
+
+    const filter = document.querySelector(".filter");
+    categories.forEach(categorie => {
+        createButtonFilter(categorie, filter);
+    });
+}
+
+function createButtonFilter(categorie, filter) {
+    const button = document.createElement("button");
+    button.innerText = categorie;
+    button.className = "filterButton";
+    filter.appendChild(button);
+}
+
+//display filter buttons
+function displayFilterButtons(categories) {
+    allButton();
+    filterButtons(categories);
+    //Event listener filter buttons
+    const filterButton = document.getElementsByClassName("filterButton");
+    filterButton.addEventListener("click", function() {
+        ////////// continuer ICI
+        });
+};
+
+
